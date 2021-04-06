c1a01ba49d585711075c71496d64a238c8442c7e
Filipe <filipefreire@protonmail.com>
2021-04-06 16:14:29 +0100

    -Add changes to 404 page and transparent option to navbar

--- pages/404.js
+++ pages/404.js
@@ -1,25 +1,30 @@
 import Link from "next/link";
-import styles from "./404.module.scss";
+import styles from "../styles/404.module.scss";
 
 const NotFound = () => {
   return (
-    <div className={styles.flexContainer}>
-      <h1 className={styles.title}>Oops...</h1>
-      <div className={styles.content}>
-        <p className={styles.paragraph}>
-          You're not supposed to be here!{" "}
-          <span role="img" aria-label="">
-            😱
-          </span>
-        </p>
-        <Link href="/">
-          <a className={styles.btn}>Head home!</a>
-        </Link>
+    <>
+      <h1 className={styles.title}>
+        <span>Error 404</span> - Page Not Found
+      </h1>
+      <div className={styles.flexContainer}>
+        <div className={styles.content}>
+          <p className={styles.subTitle}>Oops...</p>
+          <p className={styles.paragraph}>
+            Looks like the page you requested got lost in the internet void!{" "}
+            <span role="img" aria-label="">
+              😱
+            </span>
+          </p>
+          <Link href="/">
+            <a className={styles.btn}>Head home!</a>
+          </Link>
+        </div>
+        <div className={styles.notFoundImage}>
+          <img src="./404-img.png" alt="" />
+        </div>
       </div>
-      <div className={styles.notFoundImage}>
-        <img src="./404-img.png" alt="" />
-      </div>
-    </div>
+    </>
   );
 };
 
