925de15f0bcabd454b3e9aaeb9ee9b20fb46f8f1
Filipe <filipefreire@protonmail.com>
2021-04-10 22:51:31 +0100

    -Add mobile styling to contact page

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -5,48 +5,51 @@ import { motion } from "framer-motion";
 const Contact = () => {
   return (
     <>
-      <HeadComp title='Contact'>
+      <HeadComp title="Contact">
         <link
-          rel='stylesheet'
-          href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.css'
-          integrity='sha256-46qynGAkLSFpVbEBog43gvNhfrOj+BmwXdxFgVK/Kvc='
-          crossorigin='anonymous'
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.css"
+          integrity="sha256-46qynGAkLSFpVbEBog43gvNhfrOj+BmwXdxFgVK/Kvc="
+          crossorigin="anonymous"
         />
       </HeadComp>
       <motion.div
         className={styles.contactLayout}
-        initial='hidden'
-        animate='visible'
+        initial="hidden"
+        animate="visible"
         exit={{ opacity: 0 }}
         variants={{
           hidden: {
             scale: 0.8,
-            opacity: 0
+            opacity: 0,
           },
           visible: {
             scale: 1,
             opacity: 1,
             transition: {
-              delay: 0.2
-            }
-          }
+              delay: 0.2,
+            },
+          },
         }}
       >
         <div className={styles.flexContainer}>
           <div className={styles.links}>
-            <h1 className={styles.title}>
-              Reach out!{" "}
-              <span role='img' aria-label=''>
-                ✉️
-              </span>
-            </h1>
-            <p className={styles.paragraph}>Want to get in touch? You're in the right place!</p>
+            <h1 className={styles.title}>Reach out! ✉️</h1>
+            <div className={styles.messageImgMobile}>
+              <img
+                src="/assets/img/illustrations/Social-Media-Notifications.svg"
+                alt=""
+              />
+            </div>
             <p className={styles.paragraph}>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel distinctio alias
-              eligendi sint hic quibusdam itaque velit quisquam quo quasi?S
+              Whether to talk business, propose a collaboration or just speak
+              about the latest tech trends, feel free to send me a message! I'm
+              always open to connect.
             </p>
+
             <p className={styles.paragraph}>
-              I'm usually active in the following social media platforms. ⬇️
+              {/* I'm usually active in the following social media platforms. ⬇️ */}
+              Places where you can find me:
             </p>
             <ul className={styles.socialList}>
               <li>
@@ -54,11 +57,11 @@ const Contact = () => {
                   className={styles.socialListItemTwitter}
                   animate={{ scale: 2.8 }}
                   transition={{ duration: 0.5 }}
-                  href='https://twitter.com/Filipe__Freire'
-                  target='_blank'
-                  rel='noopener'
+                  href="https://twitter.com/Filipe__Freire"
+                  target="_blank"
+                  rel="noopener"
                 >
-                  <i className='fab fa-twitter fa-2x' />
+                  <i className="fab fa-twitter fa-2x" />
                 </motion.a>
               </li>
               <li>
@@ -66,11 +69,11 @@ const Contact = () => {
                   animate={{ scale: 2.8 }}
                   transition={{ duration: 0.7 }}
                   className={styles.socialListItemGithub}
-                  href='https://github.com/filipe-freire/'
-                  target='_blank'
-                  rel='noopener'
+                  href="https://github.com/filipe-freire/"
+                  target="_blank"
+                  rel="noopener"
                 >
-                  <i className='fab fa-github fa-2x' />
+                  <i className="fab fa-github fa-2x" />
                 </motion.a>
               </li>
               <li>
@@ -78,11 +81,11 @@ const Contact = () => {
                   animate={{ scale: 2.8 }}
                   transition={{ duration: 0.9 }}
                   className={styles.socialListItemLinkedIn}
-                  href='https://linkedin.com/in/filiperpfreire/'
-                  target='_blank'
-                  rel='noopener'
+                  href="https://linkedin.com/in/filiperpfreire/"
+                  target="_blank"
+                  rel="noopener"
                 >
-                  <i className='fab fa-linkedin fa-2x' />
+                  <i className="fab fa-linkedin fa-2x" />
                 </motion.a>
               </li>
               <li>
@@ -90,17 +93,20 @@ const Contact = () => {
                   animate={{ scale: 2.8 }}
                   transition={{ duration: 1.1 }}
                   className={styles.socialListItemInstagram}
-                  href='https://www.instagram.com/filipe__freire/'
-                  target='_blank'
-                  rel='noopener'
+                  href="https://www.instagram.com/filipe__freire/"
+                  target="_blank"
+                  rel="noopener"
                 >
-                  <i className='fab fa-instagram fa-2x' />
+                  <i className="fab fa-instagram fa-2x" />
                 </motion.a>
               </li>
             </ul>
           </div>
           <div className={styles.messageImg}>
-            <img src='/assets/img/illustrations/Messaging.svg' alt='' />
+            <img
+              src="/assets/img/illustrations/Social-Media-Notifications.svg"
+              alt=""
+            />
           </div>
         </div>
       </motion.div>
